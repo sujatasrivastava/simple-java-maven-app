9994f309c75eab53bb935322df96ca99d7d57214
bhanuraina <bhanuraina@gmail.com>
2019-08-29 17:56:29 +0200

    pull

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! All";
+    private final String message = "Hello World! to all";
 
     public App() {}
 
