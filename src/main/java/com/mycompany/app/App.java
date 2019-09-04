9f172304fefb9fd02977d06fe4c6a087f57a8644
bhanuraina <bhanuraina@gmail.com>
2019-09-04 17:39:56 +0200

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! to all";
+    private final String message = "Hello World! to all ";
 
     public App() {}
 
